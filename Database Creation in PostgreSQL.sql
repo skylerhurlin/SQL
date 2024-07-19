a876c35bfef47c46ab26a6938a5c332c69d3bc17
Skyler Hurlin <skylerhurlin@gmail.com>
2024-07-19 04:10:23 -0700

    Add files via upload

---
+++ Database Creation in PostgreSQL.sql
@@ -0,0 +1,185 @@
+-- Creating a database in PostgreSQL. CSV data was then uploaded using PostgreSQL's Import function.
+
+CREATE DATABASE hotel_db;
+
+CREATE TABLE "suppliers" (
+  "supplier_id" SERIAL,
+  "supplier" VARCHAR(180),
+  "service_type" VARCHAR(180),
+  "supplier_phone" VARCHAR(20),
+  "contact_name" VARCHAR(180),
+  PRIMARY KEY ("supplier_id")
+);
+
+CREATE TABLE "hotels" (
+  "hotel_id" SERIAL,
+  "hotel_name" VARCHAR(180),
+  "address" VARCHAR(180),
+  "city" VARCHAR(180),
+  "state" VARCHAR(50),
+  "website" VARCHAR(180),
+  "total_rooms" SMALLINT,
+  "front_desk_num" VARCHAR(20),
+  "venue_yn" BOOLEAN,
+  "manager_id" INTEGER,
+  PRIMARY KEY ("hotel_id")
+);
+
+CREATE TABLE "orders" (
+  "order_id" SERIAL,
+  "supplier_id" INTEGER,
+  "hotel_id" INTEGER,
+  "order_date" DATE,
+  "exp_arrival_date" DATE,
+  "arrival_date" DATE,
+  "total_paid" NUMERIC (10,2),
+  PRIMARY KEY ("order_id"),
+  CONSTRAINT "FK_orders.supplier_id"
+    FOREIGN KEY ("supplier_id")
+      REFERENCES "suppliers"("supplier_id"),
+  CONSTRAINT "FK_orders.hotel_id"
+    FOREIGN KEY ("hotel_id")
+      REFERENCES "hotels"("hotel_id")
+);
+
+CREATE TABLE "items_services" (
+  "items_services_id" SERIAL,
+  "items_services_name" VARCHAR(180),
+  "supplier_id" Type,
+  "items_services_desc" TEXT,
+  "service_type" VARCHAR(50),
+  "amt_stock" INTEGER,
+  "reorder_amt" INTEGER,
+  "reorder_yn" BOOLEAN,
+  "date_oos" DATE,
+  "last_ordered" DATE,
+  PRIMARY KEY ("items_services_id"),
+  CONSTRAINT "FK_items_services.last_ordered"
+    FOREIGN KEY ("last_ordered")
+      REFERENCES "orders"("order_date"),
+  CONSTRAINT "FK_items_services.supplier_id"
+    FOREIGN KEY ("supplier_id")
+      REFERENCES "suppliers"("supplier_id")
+);
+
+CREATE TABLE "departments" (
+  "dept_id" SERIAL,
+  "dept_name" VARCHAR(20),
+  PRIMARY KEY ("dept_id")
+);
+
+CREATE TABLE "employees" (
+  "employee_id" SERIAL,
+  "dept_id" INTEGER,
+  "hotel_id" INTEGER,
+  "emp_first_name" VARCHAR(180),
+  "emp_last_name" VARCHAR(180),
+  "title" VARCHAR(180),
+  "salary" NUMERIC (8,2),
+  "status" VARCHAR(20),
+  "phone_num" VARCHAR(20),
+  "start_date" DATE,
+  "end_date" DATE,
+  "performance_score" SMALLINT,
+  PRIMARY KEY ("employee_id"),
+  CONSTRAINT "FK_employees.dept_id"
+    FOREIGN KEY ("dept_id")
+      REFERENCES "departments"("dept_id"),
+  CONSTRAINT "FK_employees.hotel_id"
+    FOREIGN KEY ("hotel_id")
+      REFERENCES "hotels"("hotel_id")
+);
+
+CREATE TABLE "customers" (
+  "cust_id" SERIAL,
+  "cust_first_name" VARCHAR(180),
+  "cust_last_name" VARCHAR(180),
+  "email" VARCHAR(180),
+  "phone_num" VARCHAR(20),
+  "address" VARCHAR(100),
+  "city" VARCHAR(100),
+  "state_province" VARCHAR(20),
+  "country" VARCHAR(20),
+  "zip_code" VARCHAR(20),
+  "card_num" CHAR(16),
+  "vip_yn" BOOLEAN,
+  PRIMARY KEY ("cust_id")
+);
+
+CREATE TABLE "room_info" (
+  "room_type_id" SERIAL,
+  "room_type" VARCHAR(50),
+  "num_beds" SMALLINT,
+  "kitchenette_yn" BOOLEAN,
+  "smoking_yn" BOOLEAN,
+  "price" NUMERIC (8,2),
+  PRIMARY KEY ("room_type_id")
+);
+
+CREATE TABLE "reservations" (
+  "res_id" SERIAL,
+  "cust_id" INTEGER,
+  "hotel_id" INTEGER,
+  "room_type_id" INTEGER,
+  "room_num" INTEGER,
+  "guest_type" VARCHAR(20),
+  "check_in_date" DATE,
+  "check_out_date" DATE,
+  "res_method" VARCHAR(20),
+  "res_date" TIMESTAMP,
+  "base_expense" NUMERIC (8,2),
+  "extra_expenses" NUMERIC (8,2),
+  "cancelled_yn" BOOLEAN,
+  PRIMARY KEY ("res_id"),
+  CONSTRAINT "FK_reservations.room_type_id"
+    FOREIGN KEY ("room_type_id")
+      REFERENCES "room_info"("room_type_id"),
+  CONSTRAINT "FK_reservations.cust_id"
+    FOREIGN KEY ("cust_id")
+      REFERENCES "customers"("cust_id"),
+  CONSTRAINT "FK_reservations.hotel_id"
+    FOREIGN KEY ("hotel_id")
+      REFERENCES "hotels"("hotel_id")
+);
+
+CREATE TABLE "is_per_order" (
+  "(order_id, items_services_id)" (INTEGER, INTEGER),
+  "quantity" INTEGER,
+  "price" NUMERIC (8,2),
+  PRIMARY KEY ("(order_id, items_services_id)"),
+  CONSTRAINT "FK_is_per_order.(order_id, items_services_id)"
+    FOREIGN KEY ("(order_id, items_services_id)")
+      REFERENCES "items_services"("items_services_id"),
+  CONSTRAINT "FK_is_per_order.(order_id, items_services_id)"
+    FOREIGN KEY ("(order_id, items_services_id)")
+      REFERENCES "orders"("order_id")
+);
+
+CREATE TABLE "reviews" (
+  "review_id" SERIAL,
+  "res_id" INTEGER,
+  "cust_id" INTEGER,
+  "hotel_id" INTEGER,
+  "room_type_id" INTEGER,
+  "employee_id" INTEGER,
+  "overall_rating" SMALLINT,
+  "review_txt" TEXT,
+  "resolve_yn" BOOLEAN,
+  "resolved_yn" BOOLEAN,
+  PRIMARY KEY ("review_id"),
+  CONSTRAINT "FK_reviews.res_id"
+    FOREIGN KEY ("res_id")
+      REFERENCES "reservations"("res_id"),
+  CONSTRAINT "FK_reviews.cust_id"
+    FOREIGN KEY ("cust_id")
+      REFERENCES "customers"("cust_id"),
+  CONSTRAINT "FK_reviews.hotel_id"
+    FOREIGN KEY ("hotel_id")
+      REFERENCES "hotels"("hotel_id"),
+  CONSTRAINT "FK_reviews.room_type_id"
+    FOREIGN KEY ("room_type_id")
+      REFERENCES "room_info"("room_type_id"),
+  CONSTRAINT "FK_reviews.employee_id"
+    FOREIGN KEY ("employee_id")
+      REFERENCES "employees"("employee_id")
+); 
---
+++ Database Reports in PostgreSQL.sql
@@ -0,0 +1,59 @@
+-- Provides a report of items and services.
+
+SELECT DISTINCT i_s.items_services_id, items_services_name, s.supplier_name, 
+amt_stock, reorder_amt, reorder_yn,
+  	(SELECT MAX(order_date) 
+ 	 FROM is_per_order AS ipo2 
+  	JOIN orders AS o2 
+  	ON ipo2.order_id = o2.order_id
+  	WHERE ipo2.items_services_id = i_s.items_services_id) AS last_ordered
+FROM items_services AS i_s
+JOIN is_per_order AS ipo
+     ON i_s.items_services_id = ipo.items_services_id
+JOIN orders AS o
+     ON ipo.order_id = o.order_id
+JOIN suppliers AS s
+	ON o.supplier_id = s.supplier_id
+ORDER BY i_s.items_services_id;
+
+-- Provides information about employee performance.
+
+SELECT e.employee_id, emp_first_name, emp_last_name, title, hotel_name, ROUND(salary,0) AS salary, status, 
+   (SELECT COUNT(DISTINCT r2.employee_id)
+    FROM reviews AS r2
+    WHERE r2.employee_id = e.employee_id) AS number_reviews_mentioned, performance_score,
+    (CASE WHEN performance_score <7 THEN 'Y' ELSE '' END )AS reach_out_yn
+FROM employees AS e
+JOIN hotels AS h
+	ON e.hotel_id = h.hotel_id
+ORDER BY e.employee_id;
+
+
+-- Provides summary of hotel reservations.
+
+SELECT r.hotel_id, hotel_name, COUNT(res_id) AS num_res, COUNT(DISTINCT cust_id) AS unique_cust_num,
+	ROUND(AVG(check_out_date - check_in_date), 0) AS avg_stay_days,
+	COUNT(extra_expenses) AS extra_charges_num,
+	SUM(CASE WHEN cancelled_yn = 'Y' THEN 1 ELSE 0 END) AS num_cancels
+FROM reservations AS r
+JOIN hotels AS h 
+	ON r.hotel_id = h.hotel_id
+GROUP BY r.hotel_id, hotel_name
+ORDER BY r.hotel_id;
+
+-- Provides summary of overall ratings.
+
+SELECT r.hotel_id, hotel_name,  COUNT(*) AS number_reviews,
+	ROUND(AVG(overall_rating), 1) AS avg_overall_rating,
+	MAX(overall_rating) AS max_overall_rating,
+	MIN(overall_rating) AS min_overall_rating,
+	COUNT(CASE WHEN resolve_yn = 'Y' THEN 1 END) AS reviews_to_resolve,
+	COUNT (CASE WHEN resolved_yn = 'Y' THEN 1 END) AS resolved_reviews,
+	CASE WHEN AVG(overall_rating) < 4 THEN 'Y' ELSE 'N' END AS urgent_yn
+FROM reviews AS r
+JOIN hotels AS h ON r.hotel_id = h.hotel_id
+GROUP BY r.hotel_id, hotel_name
+ORDER BY r.hotel_id;
+
+
+
