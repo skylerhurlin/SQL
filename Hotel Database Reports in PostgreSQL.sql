12e4a753e1749ac8a340fcf13764713876bcc90e
Skyler Hurlin <skylerhurlin@gmail.com>
2024-08-07 03:48:12 -0700

    describing

--- Hotel Database Reports in PostgreSQL.sql
+++ Hotel Database Reports in PostgreSQL.sql
@@ -1,4 +1,6 @@
--- Provides a report of items and services.
+-- A collection of reports I wrote to pull from the database I created in the Hotel Database Creation file. I generated example data and populated the database with it.
+
+-- Report 1: Provides a report of items and services. This joins the orders, items/services per order, and suppliers tables to see the amount in stock along with where the item/service is purchased from, when the item/service was last ordered, and whether it needs to be restocked/ordered again. 
 
 SELECT DISTINCT i_s.items_services_id, items_services_name, s.supplier_name, 
 amt_stock, reorder_amt, reorder_yn,
@@ -16,7 +18,7 @@ JOIN suppliers AS s
 	ON o.supplier_id = s.supplier_id
 ORDER BY i_s.items_services_id;
 
--- Provides information about employee performance.
+-- Report 2: Provides information about employee performance. A reader can see how much employees make, if they are full-time or part-time, and if they are specifically mentioned in reviews, along with a performance score. An employee with a high score who was mentioned in reviews has stood out and may have earned a raise or promotion. An employee with a low score who was mentioned in reviews will probably need to be spoken to.
 
 SELECT e.employee_id, emp_first_name, emp_last_name, title, hotel_name, ROUND(salary,0) AS salary, status, 
    (SELECT COUNT(DISTINCT r2.employee_id)
@@ -29,7 +31,7 @@ JOIN hotels AS h
 ORDER BY e.employee_id;
 
 
--- Provides summary of hotel reservations.
+-- Report 3: Provides summary of hotel reservations by joining the hotels and reservations tables. The viewer can see how popular each hotel is and how long people stay there, how many extra expenses (ex. hotel bar, room service, extra amenities) are charged, and how many people cancel their reservations. 
 
 SELECT r.hotel_id, hotel_name, COUNT(res_id) AS num_res, COUNT(DISTINCT cust_id) AS unique_cust_num,
 	ROUND(AVG(check_out_date - check_in_date), 0) AS avg_stay_days,
@@ -41,7 +43,7 @@ JOIN hotels AS h
 GROUP BY r.hotel_id, hotel_name
 ORDER BY r.hotel_id;
 
--- Provides summary of overall ratings.
+-- Report 4: Provides summary of overall hotel ratings by joining the hotels table with the reviews table. When a hotel's rating is too low (overall_rating), it is marked as urgent. If there are reviews that have not been resolved yet, someone from customer service will reach out to the unhappy customer. 
 
 SELECT r.hotel_id, hotel_name,  COUNT(*) AS number_reviews,
 	ROUND(AVG(overall_rating), 1) AS avg_overall_rating,
